6f4309c287daf7def1a41c8271cbc4c607f12a9d
Máximo Lanza <max.slanza@gmail.com>
2022-02-10 01:09:25 -0300

    first commit

---
+++ NextJsStrapiStripe/ecomerce/server/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9a5fe6f4526b2e200074e7a2be986829'),
+    },
+  },
+});
---
+++ NextJsStrapiStripe/ecomerce/server/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '8cae025f-0579-4534-a736-5a670c06566d'
+};
